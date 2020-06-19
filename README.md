9b5fc00711b74385454571cf02a217b32e09b49d
AllAroundD <AllAroundDWeb@gmail.com>
2020-06-18 22:41:31 -0400

    finalized and added demo

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# team-generator
-team generator command line application
+<img src="https://img.shields.io/badge/AllAroundD-Team%20Generator-green">
+
+# Team-Generator
+
+## Description:
+  This is a command line application (CLI) to generate an HTML webpage with team information based on the user's responses to questions about the manager and the team members.
+
+## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+## Installation
+  There are a few npm modules that are required. inquirer (for the prompts), fs (for the file writing), chalk (to highlight text if there are errors), path (for file management).
+  Typing 'npm install' will install all of the required modules.
+  Once the user runs the program by typing 'node app.js' and answers the questions, a team.html webpage will be created in the output folder.
+
+## Usage
+  User runs the program by typing 'node app.js'. The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application will create an HTML file that displays a nicely formatted team roster based on the information provided by the user.
+
+![team generator demo](./assets/teamgenerator-demo.gif)
+
+## License
+  
+
+## Contributing
+  
+
+## Tests
+  The application uses Jest for the unit tests to test each of the employee objects. The application validates each of the input answers.
+
+## Questions
+![GitHub Profile Image](https://avatars3.githubusercontent.com/u/64918107?s=460&u=4277fa2bf868713adec524f08700cee517941e82&v=4)
+
+[GitHub Profile](https://github.com/AllAroundD/)
+
+-If you have any questions, please contact me at [dougmoore@use.startmail.com](mailto:dougmoore@use.startmail.com?subject=[GitHub]%20Source%20Question).
--- app.js
+++ app.js
@@ -1,17 +1,47 @@
+// requires
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
+const chalk = require("chalk")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
+// regular expressions to validate input
+const regexName = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/
+const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const regexPhone = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/
+const regexNumber = /[1-9]/
 
 // setting employee ID to 1 to start
 let ID = 1
+
+// function to validate name
+function validateName(inputtxt)
+{
+    return (!regexName.test(inputtxt)) ? false : true
+}
+
+// function to validate phone number
+function validatePhoneNumber(inputtxt)
+{
+    return (!regexPhone.test(inputtxt)) ? false : true
+}
+
+// function to validate number
+function validateNumber(inputtxt)
+{
+    return (!regexNumber.test(inputtxt)) ? false : true
+}
+
+// function to validate email address
+function validateEmail(inputtxt)
+{
+    return (!regexEmail.test(inputtxt)) ? false : true
+}
 
 // main function to create team and render to html file
 async function main() {
@@ -19,34 +49,156 @@ async function main() {
     const team = []
 
     const managerData = await inquirer.prompt([
-        { name: 'name', type: 'input', message: `What is the manager's name?` },
-        { name: 'email', type: 'input', message: `What is the manager's email?`},
-        { name: 'officeNumber', type: 'input', message: `What is their office number?` },
-        { name: 'count', type: 'input', message: `How many people work under them?` }
+        { 
+            name: 'name', 
+            type: 'input', 
+            message: `What is the Manager's Name?`,
+            validate: (input) => {
+                if (input == ""){
+                    return `Error: Please enter the ${chalk.red("Manager's Name")}`
+                }
+                if (!validateName(input) ){
+                    return `Error: Please enter a valid ${chalk.red("Manager's Name")}`
+                }
+                return true
+            }
+        },
+        {
+            name: 'email', 
+            type: 'input', 
+            message: `What is the Manager's email?`,
+            validate: (input) => {
+                if (input == ""){
+                    return `Error: Please enter the ${chalk.red('email address')}`
+                }
+                if ( !validateEmail(input) ){
+                    return `Error: Please enter a valid ${chalk.red("email address")}`
+                }
+                return true
+              }
+        },
+        { 
+            name: 'officeNumber', 
+            type: 'input', 
+            message: `What is their office phone number?`,
+            validate: (input) => {
+                if (input == ""){
+                    return `Error: Please enter the ${chalk.red('phone number')}`
+                }
+                if ( !validatePhoneNumber(input) ){
+                    return `Error: Please enter a valid ${chalk.red('phone number')}`
+                }
+                return true
+            }
+        },
+        { 
+            name: 'count', 
+            type: 'input', 
+            message: `How many people work under them?`,
+            validate: (input) => {
+                if (input == ""){
+                    return `Error: Please enter the ${chalk.red('number of employees')}`
+                }
+                if ( !validateNumber(input) ){
+                    return `Error: Please enter a valid ${chalk.red('number of employees')}`
+                }
+                return true
+            }
+        }
     ])
-
     // create Manager object
     team.push( new Manager(managerData.name, ID++, managerData.email, managerData.officeNumber) )
-
+    // loop through object and start to prompt questions
     for( let userCount=1; userCount <= managerData.count; userCount++ ){
-
         const user = await inquirer.prompt([
-            { name: 'type', type: 'list', message: `For person ${userCount}/${managerData.count}, what is the type of employee? `,
+            { name: 'type', type: 'list', message: `For person #${userCount} of ${managerData.count}, what is the type of employee? `,
                 choices: ["intern", "engineer" ] }
         ])
-
+        // If Engineer, prompt for the engineer questions
         if ( user.type=='engineer' ){
             const userData = await inquirer.prompt([
-                { name: 'name', type: 'input', message: `What is the engineer's name?` },
-                { name: 'email', type: 'input', message: `What is the engineer's email?`},
-                { name: 'github', type: 'input', message: `What is their github?` }
+                { 
+                    name: 'name', 
+                    type: 'input', message: `What is the Engineer's name?`, 
+                    validate: (input) => {
+                        if (input == ""){
+                            return `Error: Please enter the ${chalk.red("Engineer's Name")}`
+                        }
+                        if (!validateName(input) ){
+                            return `Error: Please enter a valid ${chalk.red("Engineer's Name")}`
+                        }
+                        return true
+                    }
+                },
+                { 
+                    name: 'email', 
+                    type: 'input', 
+                    message: `What is the Engineer's email?`,
+                    validate: (input) => {
+                        if (input == ""){
+                            return `Error: Please enter the ${chalk.red('email address')}`
+                        }
+                        if ( !validateEmail(input) ){
+                            return `Error: Please enter a valid ${chalk.red("email address")}`
+                        }
+                        return true
+                    }
+                },
+                { 
+                    name: 'github', 
+                    type: 'input', 
+                    message: `What is their Github ID?` ,
+                    validate: (input) => {
+                        if (input == ""){
+                            return `Error: Please enter the ${chalk.red('Github ID')}`
+                        }
+                        return true
+                    }
+                }
             ])
             team.push( new Engineer( userData.name, ID++, userData.email, userData.github) )
         } else {
+            // If Intern, prompt for the intern questions
             const userData = await inquirer.prompt([
-                { name: 'name', type: 'input', message: `What is the intern's name?` },
-                { name: 'email', type: 'input', message: `What is the intern's email?`},
-                { name: 'school', type: 'input', message: `What is their school?` }
+                { 
+                    name: 'name', 
+                    type: 'input', 
+                    message: `What is the Intern's name?`, 
+                    validate: (input) => {
+                        if (input == ""){
+                            return `Error: Please enter the ${chalk.red("Intern's Name")}`
+                        }
+                        if (!validateName(input) ){
+                            return `Error: Please enter a valid ${chalk.red("Intern's Name")}`
+                        }
+                        return true
+                    }
+                },
+                { 
+                    name: 'email', 
+                    type: 'input', 
+                    message: `What is the Intern's email?`,
+                    validate: (input) => {
+                        if (input == ""){
+                            return `Error: Please enter the ${chalk.red('email address')}`
+                        }
+                        if ( !validateEmail(input) ){
+                            return `Error: Please enter a valid ${chalk.red("email address")}`
+                        }
+                        return true
+                    }
+                },
+                { 
+                    name: 'school', 
+                    type: 'input', 
+                    message: `What is their school?`, 
+                    validate: (input) => {
+                        if (input == ""){
+                            return `Error: Please enter the ${chalk.red("School")}`
+                        }
+                        return true
+                    }
+                }
             ])
             team.push( new Intern( userData.name, ID++, userData.email, userData.school) )
         }
